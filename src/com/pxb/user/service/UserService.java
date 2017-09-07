117c763071368bb231f1c9aef81c12f5480aba3d
sunny717 <250553199@qq.com>
2017-09-07 11:42:43 +0800

    修改注释

--- src/com/pxb/user/service/UserService.java
+++ src/com/pxb/user/service/UserService.java
@@ -10,7 +10,7 @@ package com.pxb.user.service;
 
 /**  
  * @ClassName: UserService  
- * @Description: 用户服务层 
+ * @Description: 用户服务层  
  * @author panxiaobo  
  * @date 2017年9月7日 上午11:30:57  
  *    
