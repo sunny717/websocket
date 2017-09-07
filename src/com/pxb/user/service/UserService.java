805f705a43a485ccf828e722d48b221c6d1a2fc1
sunny717 <250553199@qq.com>
2017-09-07 11:31:26 +0800

    增加服务层

---
+++ src/com/pxb/user/service/UserService.java
@@ -0,0 +1,20 @@
+/**    
+* @Title: UserService.java  
+* @Package com.pxb.user.service  
+* @Description: TODO(用一句话描述该文件做什么)  
+* @author panxiaobo    
+* @date 2017年9月7日 上午11:30:57  
+* @version V1.0.0    
+*/  
+package com.pxb.user.service;
+
+/**  
+ * @ClassName: UserService  
+ * @Description: 用户服务层 
+ * @author panxiaobo  
+ * @date 2017年9月7日 上午11:30:57  
+ *    
+ */
+public class UserService {
+
+}
